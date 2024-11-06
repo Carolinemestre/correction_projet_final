65eaf2b4149fd3650cf0ffff1897467958d5c8f2
Caroline Mestre <carolinemestre930@gmail.com>
2024-11-06 13:55:28 +0000

    Add tests and docs to models

--- models/mart/operations/mrt_operations__daily_order_performance.sql
+++ models/mart/operations/mrt_operations__daily_order_performance.sql
@@ -1,10 +1,10 @@
 select order_date,
-    customer_city,
-    customer_state,
+    city,
+    state,
     COUNT(order_id) as total_orders,
     SUM(total_order_amount) as total_order_amount,
     AVG(total_order_amount) as avg_order_amount
 from {{ ref('int_bike_database__orders') }} 
 group by order_date,
-    customer_city,
-    customer_state
+    city,
+    state
